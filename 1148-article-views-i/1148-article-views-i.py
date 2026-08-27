d7abb2bc09dad342e9491d201eaa64ff03fffef0
Aryan Yadav <aryananitayadav13@gmail.com>
2026-08-28 00:31:20 +0530

    Time: 289 ms (55.12%), Space: 67.6 MB (25.08%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    df = views
+    fc = (df['author_id'] == df['viewer_id'])
+    df[fc]
+    return df[fc][['author_id']].drop_duplicates().sort_values(by = 'author_id').rename(columns = {'author_id':'id'})
+    
