fc506c477941a8cc8720a4ba7781cd0c1374d137
Aryan Yadav <aryananitayadav13@gmail.com>
2026-08-28 00:40:57 +0530

    Time: 306 ms (35.51%), Space: 67.5 MB (45.53%) - LeetHub

---
+++ 1683-invalid-tweets/1683-invalid-tweets.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    df = tweets
+    fc  = (df['content'].str.len() > 15)
+    df[fc]
+    return df[fc][['tweet_id']]
+    
